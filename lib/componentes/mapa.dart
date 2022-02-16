fb2c47112342ecbf9506021abb06b779a5966c55
alysontrizotto <alysontrizotto1@gmail.com>
2022-02-16 00:56:47 -0300

    Ajuste Sliding panel

--- lib/componentes/mapa.dart
+++ lib/componentes/mapa.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:sliding_sheet/sliding_sheet.dart';
 
 class Mapa extends StatefulWidget {
   static String tag = 'ListaPesquisa';
@@ -104,46 +105,6 @@ class MapaState extends State<Mapa> with OSMMixinObserver {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton:
-          Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-        FloatingActionButton(
-          elevation: 50,
-          child: Icon(Icons.add),
-          backgroundColor: Color(0xFF101427),
-          onPressed: () async {
-            mapController.zoomIn();
-          },
-          mini: true,
-        ),
-        FloatingActionButton(
-          elevation: 50,
-          child: Icon(Icons.remove),
-          backgroundColor: Color(0xFF101427),
-          onPressed: () async {
-            mapController.zoomOut();
-          },
-          mini: true,
-        ),
-        FloatingActionButton(
-          elevation: 50,
-          backgroundColor: Color(0xFF101427),
-          onPressed: () async {
-            atualyPosition();
-          },
-          child: ValueListenableBuilder<bool>(
-            valueListenable: rastreio,
-            builder: (ctx, isTracking, _) {
-              if (isTracking) {
-                return Icon(
-                  Icons.my_location,
-                  color: Colors.white,
-                );
-              }
-              return Icon(Icons.gps_off_sharp, color: Colors.deepOrangeAccent);
-            },
-          ),
-        ),
-      ]),
       appBar: AppBar(
         title: Text('Mapa'),
       ),
@@ -209,86 +170,155 @@ class MapaState extends State<Mapa> with OSMMixinObserver {
               ),
             ),
           ),
-          SingleChildScrollView(
-            child: Column(children: <Widget>[
-              SizedBox(
-                child: Card(
-                  elevation: 50,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  color: Colors.white,
-                  child: TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Pesquisa',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30)),
-                        contentPadding: const EdgeInsets.all(15),
-                      ),
-                      onChanged: (String value) {
-                        setState(() {
-                          pesquisa = value;
-                        });
-                      }),
+          Positioned(
+            bottom: 30.0,
+            right: 20.0,
+            child:
+                //floatingActionButton:
+                Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+              FloatingActionButton(
+                elevation: 50,
+                child: Icon(Icons.add),
+                backgroundColor: Color(0xFF101427),
+                onPressed: () async {
+                  mapController.zoomIn();
+                },
+                mini: true,
+              ),
+              FloatingActionButton(
+                elevation: 50,
+                child: Icon(Icons.remove),
+                backgroundColor: Color(0xFF101427),
+                onPressed: () async {
+                  mapController.zoomOut();
+                },
+                mini: true,
+              ),
+              FloatingActionButton(
+                elevation: 50,
+                backgroundColor: Color(0xFF101427),
+                onPressed: () async {
+                  atualyPosition();
+                },
+                child: ValueListenableBuilder<bool>(
+                  valueListenable: rastreio,
+                  builder: (ctx, isTracking, _) {
+                    if (isTracking) {
+                      return Icon(
+                        Icons.my_location,
+                        color: Colors.white,
+                      );
+                    }
+                    return Icon(Icons.gps_off_sharp,
+                        color: Colors.deepOrangeAccent);
+                  },
                 ),
               ),
-              Container(
-              height: MediaQuery.of(context).size.height - 263,
-              child: 
-               FutureBuilder(
-                      future: Future.delayed(Duration())
-                          .then((value) => pesquisaEndereco(pesquisa)),
-                      builder: (context, AsyncSnapshot snapshot) {
-                        if (snapshot.hasData && snapshot.data != null) {
-                          final List<SearchInfo> _retorno = snapshot.data;
-                          return ListView.builder(
-                            itemCount: _retorno.length,
-                            itemBuilder: (context, indice) {
-                              final String _endereco =
-                                  _retorno[indice].address.toString();
-                              final double lat =
-                                  _retorno[indice].point!.latitude;
-                              final double long =
-                                  _retorno[indice].point!.longitude;
-                              return Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    GestureDetector(
-                                      onTap: () {
-                                        updatePosition(lat, long);
-                                      },
-                                      child: Card(
-                                        elevation: 50,
-                                        child: ListTile(
-                                          leading:
-                                              Icon(Icons.location_on_outlined),
-                                          title: Text(_endereco),
-                                          subtitle: Text('Latitude: ' +
-                                              lat.toString() +
-                                              '\nLongitude: ' +
-                                              long.toString()),
-                                        ),
-                                      ),
-                                    ),
-                                  ]);
-                            },
-                          );
-                        } else {
-                          return Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                CircularProgressIndicator(),
-                                Text(''),
-                              ],
+            ]),
+          ),
+          SlidingSheet(
+            elevation: 20,
+            cornerRadius: 16,
+            snapSpec: const SnapSpec(
+              snap: true,
+              snappings: [20, 400, double.infinity],
+              positioning: SnapPositioning.pixelOffset,
+            ),
+            builder: (context, state) {
+              return Padding(
+                padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
+                child: Stack(children: <Widget>[
+                  Container(
+                    height: 500,
+                    child: SingleChildScrollView(
+                      child: Column(children: <Widget>[
+                        SizedBox(
+                          child: Card(
+                            elevation: 50,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
                             ),
-                          );
-                        }
-                      }),
-              
-            ),]),
-         
+                            color: Colors.white,
+                            child: TextField(
+                                decoration: InputDecoration(
+                                  hintText: 'Pesquisa',
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(30)),
+                                  contentPadding: const EdgeInsets.all(15),
+                                ),
+                                onChanged: (String value) {
+                                  setState(() {
+                                    pesquisa = value;
+                                  });
+                                }),
+                          ),
+                        ),
+                        Container(
+                          //height: MediaQuery.of(context).size.height - 263,
+                          child: FutureBuilder(
+                              future: Future.delayed(Duration())
+                                  .then((value) => pesquisaEndereco(pesquisa)),
+                              builder: (context, AsyncSnapshot snapshot) {
+                                if (snapshot.hasData && snapshot.data != null) {
+                                  final List<SearchInfo> _retorno =
+                                      snapshot.data;
+                                  return ListView.builder(
+                                    shrinkWrap: true,
+                                    physics: NeverScrollableScrollPhysics(),
+                                    itemCount: _retorno.length,
+                                    itemBuilder: (context, indice) {
+                                      final String _endereco =
+                                          _retorno[indice].address.toString();
+                                      final double lat =
+                                          _retorno[indice].point!.latitude;
+                                      final double long =
+                                          _retorno[indice].point!.longitude;
+                                      return Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: <Widget>[
+                                            GestureDetector(
+                                              onTap: () {
+                                                updatePosition(lat, long);
+                                              },
+                                              child: Card(
+                                                elevation: 50,
+                                                child: ListTile(
+                                                  leading: Icon(Icons
+                                                      .location_on_outlined),
+                                                  title: Text(_endereco),
+                                                  subtitle: Text('Latitude: ' +
+                                                      lat.toString() +
+                                                      '\nLongitude: ' +
+                                                      long.toString()),
+                                                ),
+                                              ),
+                                            ),
+                                          ]);
+                                    },
+                                  );
+                                } else {
+                                  return Center(
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        CircularProgressIndicator(),
+                                        Text(''),
+                                      ],
+                                    ),
+                                  );
+                                }
+                              }),
+                        ),
+                      ]),
+                    ),
+                  ),
+                ]),
+              );
+            },
           ),
         ]),
       ),
@@ -320,7 +350,24 @@ class MapaState extends State<Mapa> with OSMMixinObserver {
 
   Future<void> updatePosition(double lat, double long) async {
     await mapController.disabledTracking();
+    
+    
+
     await mapController.goToLocation(GeoPoint(latitude: lat, longitude: long));
+    
+    await mapController.addMarker(
+      GeoPoint(
+        latitude: lat, 
+        longitude: long
+     ),
+      markerIcon:MarkerIcon(
+        icon: Icon(
+          Icons.location_on_outlined
+        )
+      ),
+      angle:pi/3
+    );
+   
     await mapController.setZoom(zoomLevel: 17);
   }
 
